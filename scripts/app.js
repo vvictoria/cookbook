49caf93bd47a7b509671d3652f9e4ccd7a92fd03
vvictoria <viniarska.v@gmail.com>
2016-08-10 01:26:04 +0300

    add recipes

--- scripts/app.js
+++ scripts/app.js
@@ -1,11 +1,40 @@
 var cookbook = angular.module('cookbook', ["ngRoute"]);
 
+cookbook.controller('RecipesController', function($scope) {
+    $scope.recipes = [
+        {
+            title: 'Test 1',
+            date: 'yesterday',
+            image: '/images/recipes/asperges.jpg'
+        },
+        {
+            title: 'Test 2',
+            date: 'today',
+            image: '/images/recipes/asperges.jpg'
+        },
+        {
+            title: 'Test 3',
+            date: 'tomorrow',
+            image: '/images/recipes/asperges.jpg'
+        },
+    ];
+
+    $scope.addRecipe = function() {
+        $scope.recipes.push({
+            title: 'Test ' + Math.random(),
+            date: 'tomorrow',
+            image: '/images/recipes/asperges.jpg'
+        });
+    };
+});
+
 cookbook.config(["$routeProvider", "$locationProvider", function($routeProvider, $locationProvider) {
     $routeProvider.when('/', {
         templateUrl: '/pages/home.html?' + Date.now(),
     });
     $routeProvider.when('/recipes', {
-        templateUrl: '/pages/recipes.html?' + Date.now()
+        templateUrl: '/pages/recipes.html?' + Date.now(),
+        controller: 'RecipesController'
     });
     $locationProvider.html5Mode(true).hashPrefix('!');
 
