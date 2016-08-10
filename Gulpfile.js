9dc3babcb1666ba29772a20e62fa293f837033d6
vvictoria <viniarska.v@gmail.com>
2016-08-10 15:43:54 +0300

    added gulp

---
+++ Gulpfile.js
@@ -0,0 +1,16 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var minify = require('gulp-minify-css');
+var uglify = require('gulp-uglify');
+
+gulp.task('styles', function() {
+    gulp.src('styles/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./css/'));
+});
+
+gulp.task('watch',function() {
+    gulp.watch('styles/**/*.scss',['styles']);
+});
+
+gulp.task('default', ['styles', 'watch']);
