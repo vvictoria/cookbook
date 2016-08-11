acad76bf52f91535e1913ffe78cc2035d6411025
vvictoria <viniarska.v@gmail.com>
2016-08-12 01:15:18 +0300

    fixes

--- scripts/app.js
+++ scripts/app.js
@@ -6,63 +6,140 @@ function convertDate(inputFormat) {
   return pad(d.getDate()) + '/' + pad(d.getMonth()+1) + '/' + d.getFullYear() + ' ' + pad(d.getHours()) + ':' + pad(d.getMinutes()) + ':' + pad(d.getSeconds());
 };
 
-cookbook.controller('RecipesController', function($scope, $localStorage) {
+cookbook.run(function($localStorage) { // Можна перенести і вкінець, бо ств-ся при завантаж сайту?
+    window.$localStorage = $localStorage;
+
+    if (!$localStorage.recipesList) { // only first time
+        $localStorage.recipesList = [
+            {
+                id: 1,
+                name: 'Dessert with ice-cream',
+                date: '05/08/2016 18:32:45',
+                image: '/images/recipes/dessert.jpg',
+                ingredients: 'spam',
+                preparation: 'spam'
+            },
+            {
+                id: 2,
+                name: 'Asparagus pie',
+                date: '07/08/2016 13:45:01',
+                image: '/images/recipes/asperges.jpg',
+                ingredients: 'spam',
+                preparation: 'spam'
+            },
+            {
+                id: 3,
+                name: 'Burger with bacon',
+                date: '10/08/2016 15:12:37',
+                image: '/images/recipes/burger.png',
+                ingredients: 'spam',
+                preparation: 'spam'
+            },
+        ];
+        $localStorage.lastId = 3;
+    }
+});
+
+cookbook.controller('CreateRecipeController', function($scope, $localStorage, $location) {
+    $scope.saveRecipe = function() {
+        if(($scope.name == null) || ($scope.ingredients == null) || ($scope.preparation == null)) {
+            alert ('Fulfill all fields, please'); 
+        } else {
+            $localStorage.recipesList.push({
+                id: $localStorage.lastId + 1,
+                name: $scope.name,
+                date: convertDate(new Date()),
+                image: '/images/recipes/praline.jpg',
+                ingredients: $scope.ingredients,
+                preparation: $scope.preparation
+            });
+            $localStorage.lastId++;
+
+            alert('Your recipe is saved');
+            $location.path('/recipe/' + $localStorage.lastId);
+        };
+    }
+
+});
+
+cookbook.controller('RecipesListController', function($scope, $localStorage, $location) {
     $scope.recipes = $localStorage.recipesList;
-
-    $scope.addRecipe = function() {
-        $localStorage.recipesList.push({
-            title: $scope.name,
-            date: convertDate(new Date()),
-            image: '/images/recipes/praline.jpg'
-        });
-        // $localStorage.recipesList.push({})
-        alert('saved');
-        document.getElementById('name').value = '';
-        document.getElementById('ingridients').value = '';
-        document.getElementById('preparation').value = '';
-    };
-
-});
-
-cookbook.controller('FavouritesController', function($scope) {
-    $scope.openRecipe = function() {
-        window.location = '/recipe?id=${recipeId}'
+    $scope.openRecipe = function(id) {
+        $location.path('/recipe/' + id);
     };
 });
+
+var findObjectById = function (list, id) {
+    for (var i = 0; i < list.length; i++) {
+        if (id == list[i].id) {
+            return list[i];
+        }
+    }
+}
+
+cookbook.controller('EditRecipeController', function($scope, $localStorage, $routeParams, $location) {
+    var recipe = findObjectById($localStorage.recipesList, $routeParams.id);
+    $scope.name = recipe.name;
+    $scope.ingredients = recipe.ingredients;
+    $scope.preparation = recipe.preparation; 
+    $scope.saveRecipe = function() {
+        recipe.name = $scope.name;
+        recipe.ingredients = $scope.ingredients;
+        recipe.preparation = $scope.preparation;
+        $location.path('/recipe/' + recipe.id);
+    }
+});
+
+cookbook.controller('ViewRecipeController', function($scope, $localStorage, $routeParams, $location) {
+    var recipe = findObjectById($localStorage.recipesList, $routeParams.id);
+    $scope.recipe = recipe;
+    $scope.edit = function() {
+        $location.path('/recipe/' + recipe.id + '/edit');
+    }
+});
+
 
 cookbook.config(["$routeProvider", "$locationProvider", function($routeProvider, $locationProvider) {
     $routeProvider.when('/', {
-        templateUrl: '/pages/home.html?' + Date.now(),
+        templateUrl: '/pages/home.html'
     });
     $routeProvider.when('/favourites', {
-        templateUrl: '/pages/favourites.html?' + Date.now(),
-        controller: 'RecipesController'
+        templateUrl: '/pages/favourites.html',
+        controller: 'RecipesListController'
     });
     $routeProvider.when('/create', {
-        templateUrl: '/pages/create.html?' + Date.now(),
-        controller: 'RecipesController'
+        templateUrl: '/pages/create.html',
+        controller: 'CreateRecipeController'
     });
-    $locationProvider.html5Mode(true).hashPrefix('!');
+    $routeProvider.when('/recipe/:id', {
+        templateUrl: '/pages/recipe.html',
+        controller: 'ViewRecipeController'
+    });
+    $routeProvider.when('/recipe/:id/edit', {
+        templateUrl: '/pages/create.html',
+        controller: 'EditRecipeController'
+    });
+
+
+    // $routeProvider.when('/articles/:category/:date/:id', {
+    //     templateUrl: '/pages/create.html',
+    //     controller: 'ViewRecipeController'
+    // });
+
+
+    // site.com/articles/news/12-10-2009/123897
+
+
+
+    // {
+    //     category: "news",
+    //     date: "12-10-2009",
+    //     id: "123897"
+    // }
+
+
+
+    // $locationProvider.html5Mode(false).hashPrefix('!');
 }]);
 
-cookbook.run(function($localStorage) {
-    if (!$localStorage.recipesList) {
-        $localStorage.recipesList = [
-            {
-                title: 'Dessert with ice-cream',
-                date: '05/08/2016 18:32:45',
-                image: '/images/recipes/dessert.jpg'
-            },
-            {
-                title: 'Asparagus pie',
-                date: '07/08/2016 13:45:01',
-                image: '/images/recipes/asperges.jpg'
-            },
-            {
-                title: 'Burger with bacon',
-                date: '10/08/2016 15:12:37',
-                image: '/images/recipes/burger.png'
-            },
-        ];
-    }
-});
+// scope обмежужється одним шаблоном - r. 1 контроллер на 1 html В ОДИН МОМЕНТ ЧАСУ
